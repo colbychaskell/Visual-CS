a7cb0b25bfeed860126ad2ff4e42641b84a64ebd
Colby Haskell <cchaskel@usc.edu>
2021-06-08 19:07:27 -0700

    margin tweak

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ function showMenu() {
   console.log(document.body);
   if(dropDown.style.display != "block") {
     dropDown.style.display = "block";
+    dropDown.style.marginBottom = "20px";
   }
   else {
     dropDown.style.display = "none";
