54bf7e0b69799c22c8a2dac27e3ac79f1d7f143a
Colby Haskell <cchaskel@usc.edu>
2021-06-08 20:24:15 -0700

    Button Selection

--- js/index.js
+++ js/index.js
@@ -1,4 +1,14 @@
-function showMenu() {
+window.addEventListener('load', init, false);
+
+function init(){
+  document.getElementById('topicBtn').onclick = showDropdown;
+  document.getElementById('data').onclick = setTopicData;
+  document.getElementById('graph').onclick = setTopicGraph;
+  document.getElementById('trees').onclick = setTopicTrees;
+
+}
+
+function showDropdown() {
   var dropDown = document.getElementById("dropMenu");
   console.log(document.body);
   if(dropDown.style.display != "block") {
@@ -8,4 +18,31 @@ function showMenu() {
   else {
     dropDown.style.display = "none";
   }
+}
+
+function setTopicData() {
+  var dropDown = document.getElementById("dropMenu");
+  dropDown.style.display = "none";
+  var topicBtn = document.getElementById('topicBtn');
+
+  topicBtn.innerHTML = "Data Structures";
+
+}
+
+function setTopicGraph() {
+  var dropDown = document.getElementById("dropMenu");
+  dropDown.style.display = "none";
+  var topicBtn = document.getElementById('topicBtn');
+
+  topicBtn.innerHTML = "Graph Search Algorithms";
+
+}
+
+function setTopicTrees() {
+  var dropDown = document.getElementById("dropMenu");
+  dropDown.style.display = "none";
+  var topicBtn = document.getElementById('topicBtn');
+
+  topicBtn.innerHTML = "Trees";
+
 }
